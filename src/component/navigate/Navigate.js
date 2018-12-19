1d17b9342adea5dd50bd927f49a714bf05625482
danacm <chen_dana@sina.cn>
2018-12-19 14:10:21 +0800

    增加菜单和下拉列表

--- src/component/navigate/Navigate.js
+++ src/component/navigate/Navigate.js
@@ -1,17 +1,49 @@
-import React, {PureComponent} from 'react';
-import {NavigateWrapper} from './style';
+/*
+ * @Author: dana_chen
+ * @Date:   2018-11-26 11:52:08
+ * @Last Modified by: dana_chen@sina.cn
+ * @Last Modified time: 2018-12-12 14:13:37
+ */
+import React, { Component } from "react";
+import { Menu, Icon } from "antd";
+import 'antd/dist/antd.css';
+// components
+export default class SideNav extends Component {
+  loop = (data) =>
+    data.map(item => {
+      //let nav = null;
+      if (item.items && item.items.length) {
+        return (
+          <Menu.SubMenu
+            key={item.id}
+            title={
+              <span>
+                <Icon type={item.icon} /> <span> {item.label} </span>
+              </span>
+            }>
+            {this.loop(item.items)}
+          </Menu.SubMenu>
+        );
+      } else {
+        return (
+          <Menu.Item key={item.id}>
+            <Icon type={item.icon} />
+            <span>{item.label}</span>
+          </Menu.Item>
+        );
+      }
+      //return { nav };
+    });
 
-function test(){
-
+  render() {
+    const { menuData, ...param } = this.props;
+    return (
+      <Menu
+        {...param}
+      >
+        {this.loop(menuData)}
+      </Menu>
+    );
+  }
 }
 
-@test
-class Navigate extends PureComponent{
-
-    render(){
-
-        return <NavigateWrapper>这是导航组件</NavigateWrapper>
-    }
-}
-
-export default Navigate;
--- src/component/navigate/index.js
+++ src/component/navigate/index.js
@@ -1,5 +1,81 @@
-import React from 'react';
+import React,{PureComponent}from 'react';
 import ReactDOM from 'react-dom';
 import Naviagte from './Navigate';
 
-ReactDOM.render(<Naviagte/>, document.getElementById("root"));
+const menuItems = [{
+    id: 1,
+    label: 'Item 1',
+    icon: 'setting',
+    items: [{
+      id: 11,
+    label: 'Item 1.1',
+      icon: 'area-chart',
+      link: '/item11',
+    }, {
+      id: 12,
+      label: 'Item 1.2',
+      icon: 'pie-chart',
+      link: '/item12',
+    }, ],
+  }, {
+    id: 2,
+    label: 'Item 2',
+    icon: 'bar-chart',
+    link: '/item2',
+  }, {
+    id: 3,
+    label: 'Item 3',
+    icon: 'bar-chart',
+    link: '/item3',
+  }, {
+    id: 4,
+    label: 'Item 4',
+    icon: 'picture',
+    items: [{
+      id: 41,
+      label: 'Item 4.1',
+      icon: 'camera',
+      items: [{
+        id: 411,
+        label: 'Item 4.1.a',
+        icon: 'eye',
+        link: '/item41/a',
+      }, {
+        id: 412,
+        label: 'Item 4.1.b',
+        icon: 'video-camera',
+        link: '/item41/b',
+      }, ],
+    }, {
+      id: 42,
+      label: 'Item 4.2',
+      icon: 'message',
+      link: '/item42',
+    }, {
+      id: 43,
+      label: 'Item 4.3',
+      icon: 'compass',
+      link: '/item43',
+    }, ],
+  }, ];
+  
+  const clickTitle = (e) => {
+    console.log(`value:`, e);
+};
+   
+class Testcontainer extends PureComponent {
+    render() {
+      return (
+        <div style={{ maxWidth: '13.75rem', padding: 0 }}>
+          <Naviagte
+            menuData={menuItems}
+            mode="inline"
+            theme="dark"
+            style={{ width: '100%' }}
+            onSelect={clickTitle}
+          />
+        </div>
+      );
+    }
+  }
+ReactDOM.render(<Testcontainer/>, document.getElementById("root"));
